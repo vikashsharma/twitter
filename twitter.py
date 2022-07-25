4b4cffdfe73c99e743c75f9243dd20561017ffc8
Vikash Sharma <vikashsharma@users.noreply.github.com>
2022-07-25 15:18:28 +0530

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -1,72 +1,86 @@
-import os
-import tweepy
-import pandas as pd
+# Install Annoconda https://docs.conda.io/en/latest/miniconda.html
+# Install and do select add to envirnment PATH option.
+# After successful installation. Run following command in command prompt:
+# > pip install tweepy pandas textblob tqdm contractions unicodedata emoji itertools
+# After this goto file location and type in command. Dont forgot to update the token and hashtag
+# > python twitter.py
+
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import tweepy as tw  # To extarct the twitter data
+from tqdm import tqdm
 import re
-from textblob import TextBlob 
-from bs4 import BeautifulSoup
+from textblob import TextBlob
+import contractions
+import unicodedata
+import emoji
+import itertools
 from pandas import json_normalize
-from nltk.tokenize import WordPunctTokenizer
 
 consumer_key = "<copy from twitter account>"
 consumer_secret = "<copy from twitter account>"
-oauth_token = "<copy from twitter account>"
-oauth_token_secret = "<copy from twitter account>"
-hash_tag = "plastic"
+final_csv = 'tweet.csv'
 num_of_record = 10
-file_path = 'sample.csv'
-search_list = ['plastic','bomb']
+search_words = "#plastic -filter:retweets"
 
-
-tok = WordPunctTokenizer()    
-pat1 = r'@[A-Za-z0-9]+'
-pat2 = r'https?://[A-Za-z0-9./]+'
-combined_pat = r'|'.join((pat1, pat2))
-
-def get_data(hashtag,limit):
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(oauth_token, oauth_token_secret)
-    api = tweepy.API(auth, wait_on_rate_limit=True)
+def get_tweets():
+    auth = tw.OAuthHandler(consumer_key, consumer_secret)
+    api = tw.API(auth, wait_on_rate_limit=True)
+    # Collect tweets
     temp = []
-    for tweet in tweepy.Cursor(api.search,q=hashtag).items(limit):
+    for tweet in tw.Cursor(api.search_tweets,
+                q=search_words,
+                lang="en"
+                ).items(num_of_record):#Return maximum of 10 tweets
         temp.append(tweet._json)
-    df = json_normalize(temp)
+    # Transform to pandas Dataframe
+    df = json_normalize(temp)  
     return df
 
-def tweet_cleaner(text):
-    soup = BeautifulSoup(text, 'lxml')
-    souped = soup.get_text()
-    stripped = re.sub(combined_pat, '', souped)
-    try:
-        clean = stripped.decode("utf-8-sig").replace(u"\ufffd", "?")
-    except:
-        clean = stripped
-    letters_only = re.sub("[^a-zA-Z]", " ", clean)
-    lower_case = letters_only.lower()
-    words = tok.tokenize(lower_case)
-    return (" ".join(words)).strip()
+def clean_text(text):
+    '''Return clean text'''
+    # remove extra space
+    text = ' '.join(text.split())
+    # replace accented characters é..
+    text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
+    text = emoji.demojize(text)# convert symbol based to text
+    text = ' '.join(re.sub(r'https?://\S+', '', text).split())# Remove link
+    text = re.sub('@[\w]+','',text)# Remove @ user
+    text =' '.join(re.sub('[^a-zA-Z0-9 \n\.]', '', text).split())#remove special character à¸£à¸­ plastic plastic à¸šà¸¸à¹‹à¸‡à¹†"
+    text = ' '.join(re.sub(r'\s+', " ", text).split())#remove space and split
+    text = re.sub(r'(?:^| )\w(?:$| )', ' ', text).strip()#remove single word
+    text = re.sub(r'[^\w\s]', '', text)#remove punc
+    text =''.join([i for i in text if not i.isdigit()])#remove digit
+    text = re.sub(r"[a-zA-Z]",lambda x :x.group(0).lower()#lower case 
+            if x.group(0).islower()
+            else x.group(0).lower(),text)
+    text = contractions.fix(text) # Replace contractions eg. wouldn't, wasn't
+    #Fix misspelled words with 2 plus repition of same character like appple to apple
+    text = ''.join(''.join(s)[:2] for _, s in itertools.groupby(text))
+    return text
 
-def get_tweet_polarity(tweet): 
-    analysis = TextBlob(tweet) 
+def get_tweet_polarity(tweet):
+    '''Return tweet polarity.'''
+    analysis = TextBlob(tweet)
     return analysis.sentiment.polarity
 
-def get_tweet_sentiment(polarity): 
-    if polarity > 0: 
+def get_tweet_sentiment(polarity):
+    '''Return sentiment to positive ,negative and neutral base on polarity score'''
+    if polarity > 0:
         return 'positive'
-    elif polarity == 0: 
+    elif polarity == 0:
         return 'neutral'
-    else: 
+    else:
         return 'negative'
 
-def find_words(s):
-    tags = []
-    for each in search_list:
-        if re.findall(r'{}\s+'.format(each),s,re.I):
-            tags.append(each)
-    return ', '.join(tags)
-
-dataset = get_data(hash_tag,num_of_record)
-dataset['cleaned_text']       = dataset.apply(lambda row :tweet_cleaner(row['text']),axis=1)
-dataset['sentiment_polarity'] = dataset.apply(lambda row :get_tweet_polarity(row['cleaned_text']),axis=1)
-dataset['sentiment']          = dataset.apply(lambda row :get_tweet_sentiment(row['sentiment_polarity']),axis=1)
-dataset['labels']             = dataset.apply(lambda row :find_words(row['cleaned_text']),axis=1)
-dataset.to_csv(file_path, index = False)
+# Get raw tweets from twitter
+df = get_tweets()
+# Drop duplicate columns
+df = df.drop_duplicates(subset='text', keep='first')
+# Cleaning unwanted text
+df['cleaned_text'] = df.apply(lambda row: clean_text(row['text']), axis=1)
+# Sentiment polarity score
+df['sentiment_polarity'] = df.apply(lambda row: get_tweet_polarity(row['cleaned_text']), axis=1)
+# Sentiment
+df['sentiment'] = df.apply(lambda row: get_tweet_sentiment(row['sentiment_polarity']), axis=1)
+# Save to csv file
+df.to_csv(final_csv, index=False)
